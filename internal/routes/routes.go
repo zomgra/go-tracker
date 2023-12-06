84c7b025bec4db3a0e7ae29494ae0b737bae2f53
Zomgra <hiban.com@gmail.com>
2023-12-06 11:47:40 +0100

    Add test gloabal and local middlewares

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,6 +1,11 @@
 package routes
 
 import (
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
 	"github.com/gorilla/mux"
 	"github.com/zomgra/bitbucket/internal/shipment"
 )
@@ -8,9 +13,32 @@ import (
 func CreateRoute() *mux.Router {
 	r := mux.NewRouter()
 
+	r.Use(logMiddlware)
+
 	//Add Shipment route
 	r.HandleFunc("/api/shipment/{id}", shipment.CheckShipments).Methods("GET")
-	r.HandleFunc("/api/shipment", shipment.CreateShipments).Methods("POST")
+	r.Handle("/api/shipment", checkQuantity(shipment.CreateShipments)).Methods("POST")
 
 	return r
+}
+
+func logMiddlware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s - %s (%s) Time beetween excecuting: %v \n", r.Method, r.URL.Path, r.RemoteAddr, time.Since(start).Milliseconds())
+	})
+}
+
+func checkQuantity(f http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		quantity, err := strconv.Atoi(r.URL.Query().Get("quantity"))
+		if err != nil {
+			log.Panic(err)
+		}
+		if quantity < 0 {
+			log.Fatal("quantity lower 0")
+		}
+		f(w, r)
+	})
 }
