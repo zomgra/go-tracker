759ab9f3215a213ffe7eae960e54d7e9cd004f5a
Zomgra <hiban.com@gmail.com>
2023-12-18 21:39:11 +0100

    Test directories change

--- test/barcodegenerator_test.go
+++ test/barcodegenerator_test.go
@@ -1,20 +1,21 @@
-package barcode
+package test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/zomgra/tracker/pkg/barcode"
 )
 
 func TestBarcodeGeneratorMustBe20LengthWith4Chars(t *testing.T) {
-	barcode := GenerateBarCode(2, 2)
+	barcode := barcode.GenerateBarCode(2, 2)
 
 	require.NotEmpty(t, barcode)
 	require.Len(t, barcode, 20)
 }
 
 func TestBarcodeGeneratorMustBe20LengthWith5Chars(t *testing.T) {
-	barcode := GenerateBarCode(2, 3)
+	barcode := barcode.GenerateBarCode(2, 3)
 
 	require.NotEmpty(t, barcode)
 	require.Len(t, barcode, 20)
--- test/main_test.go
+++ test/main_test.go
@@ -1,4 +1,4 @@
-package postgres
+package test
 
 import (
 	"fmt"
